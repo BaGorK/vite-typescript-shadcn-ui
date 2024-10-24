a5136485f2df801dc73bab64895381f9b314f41f
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-10-24 12:06:57 +0300

    feat: Add ModeToggle component
    - Import ModeToggle component in App.tsx
    - Create ModeToggle component with theme toggle functionality
    - Add ModeToggle component to App component
    - Update index.css with new color variables for theme
    - Create ThemeProvider component for managing theme state
    - Use ThemeProvider in main.tsx to wrap App component
    - Update package.json to include "@radix-ui/react-dropdown-menu" dependency
    - Remove unused "next-themes" dependency from package.json
    - Update components.json to set "cssVariables" to false

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from './components/ModeToggle';
 import { Button } from './components/ui/button';
 
 function App() {
   return (
     <div className='text-red-500 mt-36 text-center text-5xl'>
+      <ModeToggle />
       <Button>App</Button>
     </div>
   );
