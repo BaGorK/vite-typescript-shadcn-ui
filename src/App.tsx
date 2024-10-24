d094c0513eb297447081d43769455941842be8ac
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-10-24 10:09:41 +0300

    init: vite react typescript shadcn

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <div className='text-red-500 mt-36 text-center text-5xl'>
+      <Button>App</Button>
+    </div>
+  );
+}
+
+export default App;
